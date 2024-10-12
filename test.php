6c020b891d63597abe6fb7cdd7f00981b983957e
dawang-cn <XianCDawg@gmail.com>
2024-10-12 09:32:58 +0800

    Last commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Christian Dawang</title>
+</head>
+<body>
+ 
+ <?php
+    echo "Hi am Christian of the D. my idol is The Lion"
+ ?>
+
+</body>
+</html>
